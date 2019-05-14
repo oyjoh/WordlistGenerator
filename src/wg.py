5e6c243d44e21774d926f04f885764d9bb6bf59e
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2019-05-14 14:43:02 +0200

    now possible to append wordlist-top4800-probable.txt at the end

--- src/wg.py
+++ src/wg.py
@@ -8,37 +8,42 @@ version = '1.0'
 def help():
     print("\n## WORDLIST GENERATOR ##\n")
     print("[ Options ]\n")
-    print("-o\tname output file. example.txt")
-    print("-k\tkeywords separated by commas. do not use spaces")
+    print("-o\tName output file. Remember .txt or .csv")
+    print("-k\tKeywords separated by commas. Do not use spaces")
+    print('-i\tInclude "wordlist-top4800-probable.txt" at the end of your custom list')
     print("-v\tVersion of the program\n")
     sys.exit()
 
 
 def main(argv):
+    include_top = False
     output = ''
     seeds = ''
     try:
-        opts, args = getopt.getopt(argv, "hvk:o:", ["seeds=", "output="])
+        opts, args = getopt.getopt(argv, "hvik:o:", ["seeds=", "output="])
     except getopt.GetoptError:
         print("invalid command")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-v':
-            print(version)
+            print("Version: " + version)
+            sys.exit()
         elif opt == '-h':
             help()
+        elif opt == '-i':
+            include_top = True
         elif opt in "-k":
             seeds = arg
         elif opt in "-o":
             output = arg
     keywords = seeds.split(",")
     if len(keywords) > 0 and len(output) > 0:
-        generate_file(keywords, output)
+        generate_file(keywords, output, include_top)
     else:
         sys.exit("lack of arguments")
 
 
-def generate_file(keys, filename):
+def generate_file(keys, filename, include_top):
     _file = open(filename, "w")
     number_of_lines = 0
 
@@ -62,6 +67,13 @@ def generate_file(keys, filename):
         for x in range(101, 1000):
             _file.write(key.capitalize() + str(x) + "\n")
             number_of_lines += 1
+
+        if include_top:
+            with open("wordlist-top4800-probable.txt") as infile:
+                for line in infile:
+                    _file.write(line)
+                    number_of_lines += 1
+
     _file.close()
     print("\nFile successfully created!\n")
     print("Filepath: " + str(os.getcwd()) + "/" + str(filename))
